0c08c04b590912fdaddc06b586602fc12e59b675
Fangjian Lei <leix5490@gmail.com>
2021-04-08 15:06:29 -0400

    update sql

--- bookstore.sql
+++ bookstore.sql
@@ -9,11 +9,14 @@ CREATE TABLE BookStore (
   SellerLocation varchar(255) NOT NULL default '',
   Price int(255) NOT NULL default 0,
   Email varchar(255) NOT NULL default '',
+  bookURL VARCHAR(2083) default 'https://www.designsponge.com/wp-content/uploads/2011/07/final.jpeg',
+  category int(255) NOT NULL  default 1 CHECK (category>=1 AND category<=5),
+  NOF int(255) NOT NULL default 0,
   PRIMARY KEY (Id)
 );
 
 
-INSERT INTO BookStore  VALUES (1, 'Computing', 'Yulin Xue', 'Waterloo', 200, 'xuex1230@mylaurier.ca');
-INSERT INTO BookStore  VALUES (2, 'Compiler', 'Fangjian Let', 'Kitchener', 100, 'leix1111@mylaurier.ca');
-INSERT INTO BookStore  VALUES (3, 'Python', 'Xiaohu He', 'Toronto', 300, 'hexx2222@mylaurier.ca');
-INSERT INTO BookStore  VALUES (4, 'Python', 'Zheming Xing', 'Brantford', 200, 'xing333@mylaurier.ca');
+INSERT INTO BookStore  VALUES (1, 'Computing', 'Yulin Xue', 'Waterloo', 200, 'xuex1230@mylaurier.ca','https://image.isu.pub/150116060033-5618526743569a5619e7f82c0f5d3c51/jpg/page_1.jpg',default,default);
+INSERT INTO BookStore  VALUES (2, 'Compiler', 'Fangjian Let', 'Kitchener', 100, 'leix1111@mylaurier.ca','https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1526895342l/40186238._SY475_.jpg',default,default);
+INSERT INTO BookStore  VALUES (3, 'Python', 'Xiaohu He', 'Toronto', 300, 'hexx2222@mylaurier.ca','https://www.sarahsbookshelves.com/wp-content/uploads/sites/228/2021/03/Spring-2021-Book-Preview-Pinterest.jpg',default,default);
+INSERT INTO BookStore  VALUES (4, 'Python', 'Zheming Xing', 'Brantford', 200, 'xing333@mylaurier.ca','https://smartcdn.prod.postmedia.digital/vancouversun/wp-content/uploads/2020/02/0315_spring_arts_preview_books-s-1.jpg?w=288&quality=90&strip=all',default,default);
--- bookstore_user.sql
+++ bookstore_user.sql
@@ -4,12 +4,13 @@
 
 CREATE TABLE BookStoreUser (
   Id int(11) NOT NULL auto_increment,
-  UserName varchar(255) NOT NULL default '',
+  UserName varchar(255) UNIQUE NOT NULL default '',
   Password varchar(255) NOT NULL default '',
+  favorites blob,
   PRIMARY KEY (Id)
 );
 
 
-INSERT INTO BookStoreUser  VALUES (1, 'Elijah', '1234');
-INSERT INTO BookStoreUser  VALUES (2, 'Ray', 'abcd');
-INSERT INTO BookStoreUser  VALUES (3, 'Xiaohu', '9876');
+INSERT INTO BookStoreUser  VALUES (1, 'Elijah', '1234', COLUMN_CREATE('id', 1));
+INSERT INTO BookStoreUser  VALUES (2, 'Ray', 'abcd',COLUMN_CREATE('id', 3));
+INSERT INTO BookStoreUser  VALUES (3, 'Xiaohu', '9876',COLUMN_CREATE('id', 2));
